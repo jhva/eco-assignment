e4907edd1aff885ceb911dc8021013e3b8ae483a
jhva <junghun8158@naver.com>
2023-12-28 17:23:23 +0900

    :tada: build: init project

---
+++ src/main/java/com/api/ecoassignment/EcoAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.api.ecoassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ecoassignment/EcoAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ecoassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
