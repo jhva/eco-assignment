0c2da7e7717a0997af11678ba2644a6b71599eb7
jhva <junghun8158@naver.com>
2023-12-30 22:03:21 +0900

    :white_check_mark: test(employeeControllerTest): 특정사원 컨트롤러 테스트 추가

--- src/main/java/com/api/ecoassignment/domain/employee/api/EmployeeController.java
+++ src/main/java/com/api/ecoassignment/domain/employee/api/EmployeeController.java
@@ -1,5 +1,29 @@
 package com.api.ecoassignment.domain.employee.api;
 
-public class EmployeeController {
+import static org.springframework.http.HttpStatus.OK;
+
+import com.api.ecoassignment.domain.employee.application.EmployeeService;
+import com.api.ecoassignment.domain.employee.dto.response.EmployeeResponse;
+import com.api.ecoassignment.global.format.apiResponse.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/employee")
+@RequiredArgsConstructor
+class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping("/find-specific/{id}")
+    public ResponseEntity<ApiResponse> findSpecificEmployee(@PathVariable Long id) {
+        EmployeeResponse employeeResponse = employeeService.findSpecificEmployee(id);
+        return ResponseEntity.status(OK)
+                .body(ApiResponse.toSuccessForm(employeeResponse));
+    }
 
 }
---
+++ src/test/java/com/api/ecoassignment/domain/employee/api/EmployeeControllerTest.java
@@ -0,0 +1,62 @@
+package com.api.ecoassignment.domain.employee.api;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.api.ecoassignment.config.restdocs.AbstractRestDocsTests;
+import com.api.ecoassignment.domain.employee.application.EmployeeService;
+import com.api.ecoassignment.domain.employee.dto.response.EmployeeResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
+@WebMvcTest(EmployeeController.class)
+public class EmployeeControllerTest extends AbstractRestDocsTests {
+
+    private static final String EMPLOYEE_URL = "/api/v1/employee";
+
+    @Autowired
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Test
+    void 특정_사원의_현재_정보를_조회한다() throws Exception {
+
+        EmployeeResponse employee = EmployeeResponse.builder()
+                .employeeId(100L)
+                .firstName("Steven")
+                .lastName("King")
+                .email("SKING")
+                .phoneNumber("515.123.4567")
+                .managerId(null)
+                .jobId("AD_PRES")
+                .salary(BigDecimal.valueOf((float) 24000.00))
+                .departmentId(90L)
+                .hireDate(LocalDate.of(1987, 06, 17)).build();
+        given(employeeService.findSpecificEmployee(100L)).willReturn(employee);
+
+        EmployeeResponse employeeResponse = employeeService.findSpecificEmployee(100L);
+
+        mockMvc.perform(get(EMPLOYEE_URL + "/find-specific/100")
+                        .contentType(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(employeeResponse)))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        Assertions.assertEquals(employee.getEmployeeId(), employeeResponse.getEmployeeId());
+        Assertions.assertEquals(employee.getFirstName(), employeeResponse.getFirstName());
+        Assertions.assertEquals(employee.getLastName(), employeeResponse.getLastName());
+        Assertions.assertEquals(employee.getEmail(), employeeResponse.getEmail());
+
+    }
+}
