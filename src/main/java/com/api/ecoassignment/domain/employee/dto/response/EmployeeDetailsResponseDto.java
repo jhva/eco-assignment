1a6d19b29c68a991ff99abf832526b28c58cbfb6
jhva <junghun8158@naver.com>
2024-01-04 15:37:40 +0900

    :sparkles: feat: EmployeeDetailsResponseDto 클래스 생성

---
+++ src/main/java/com/api/ecoassignment/domain/employee/dto/response/EmployeeDetailsResponseDto.java
@@ -0,0 +1,36 @@
+package com.api.ecoassignment.domain.employee.dto.response;
+
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class EmployeeDetailsResponseDto {
+
+    private String email;
+    private String firstName;
+    private BigDecimal commissionPct;
+    private String lastName;
+    private String phoneNumber;
+    private Long employeeId;
+    private BigDecimal salary;
+    private String jobId;
+    private String jobTitle;
+    private Integer maxSalary;
+    private Integer minSalary;
+    private Long departmentId;
+    private String departmentName;
+    private Long locationId;
+    private String city;
+    private String postalCode;
+    private String stateProvince;
+    private String streetAddress;
+    private String countryId;
+    private String countryName;
+    private Long regionId;
+    private String regionName;
+}
